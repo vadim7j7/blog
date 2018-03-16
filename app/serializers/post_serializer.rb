4e0662a8adcf551435905acf179da010906df095
vadim7j7 <vadim7j7@gmail.com>
2018-03-17 00:01:36 +0600

    Remove initialize from post serializer

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -2,13 +2,6 @@ class PostSerializer < ActiveModel::Serializer
   attributes :id, :title, :body, :slug, :tag_list, :comments, :created_at
   has_one :profile, key: :user
 
-  def initialize(object, options={})
-    meta_key = options[:meta_key] || :meta
-    options[meta_key] ||= {}
-
-    super(object, options)
-  end
-
   def comments
     object.comments.all.arrange_serializable do |parent, children|
       CommentSerializer.new(parent, scope: { children: children })
